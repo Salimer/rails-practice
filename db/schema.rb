8bb1b2b8d71c44389ac05349ed83c04b4ad92e48
Salim-Bamahfoodh <salim.omer@jisr.net>
2025-07-08 14:44:54 +0300

    Add friendly_id package

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_07_08_063758) do
+ActiveRecord::Schema[8.0].define(version: 2025_07_08_110101) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -23,6 +23,8 @@ ActiveRecord::Schema[8.0].define(version: 2025_07_08_063758) do
     t.string "style"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "slug"
+    t.index ["slug"], name: "index_trees_on_slug", unique: true
   end
 
   create_table "users", force: :cascade do |t|
