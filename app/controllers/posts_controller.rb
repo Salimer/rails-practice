bf00c4377ebbb4fa15e6345ee5d0a94f0b965a41
Salim-Bamahfoodh <salim.omer@jisr.net>
2025-07-23 10:39:28 +0300

    checkpiont

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,13 +1,16 @@
 class PostsController < ApplicationController
   before_action :set_post, only: %i[ show edit update destroy ]
+  before_action :track_visit_count, only: [ :index ]
 
   # GET /posts or /posts.json
   def index
+    logger.info "Fetching all posts at #{Time.now}"
     @posts = Post.all
   end
 
   # GET /posts/1 or /posts/1.json
   def show
+    session[:last_viewed_post_id] = @post.id
   end
 
   # GET /posts/new
@@ -58,13 +61,19 @@ class PostsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_post
-      @post = Post.find(params.expect(:id))
-    end
 
-    # Only allow a list of trusted parameters through.
-    def post_params
-      params.expect(post: [ :title, :body ])
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_post
+    @post = Post.find(params.expect(:id))
+  end
+
+  # Only allow a list of trusted parameters through.
+  def post_params
+    params.expect(post: [ :title, :body ])
+  end
+
+  def track_visit_count
+    cookies[:visit_count] = cookies[:visit_count].to_i + 1
+    @visit_count = cookies[:visit_count]
+  end
 end
--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,2 +1,5 @@
 module PostsHelper
+  def show_visit_count(count)
+    "You have visited this page #{count} times."
+  end
 end
